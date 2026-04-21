bce1e83d1d94341cd0a34bd212fb2747c4ed52b9
jm-tanador <jmtanador7@gmail.com>
2026-04-21 09:27:37 +0800

    Fix workflow filename and content

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+    base: '/real-estate/',
     plugins: [
         vue(),
         laravel({
